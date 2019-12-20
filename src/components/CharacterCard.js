7f006694641b5f8329cf3d4c2ec78b2825eccb14
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-12-20 13:29:22 -0500

    finally got characters and images to render and implemented a search...MVP met

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Card = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  border: 1px solid #222;
+  margin: 5px;
+  width: 100%;
+  height: 400px;
+  font-size: 16px;
+  box-shadow: 5px 6px 5px #333;
+
+`
+
+
+export default function CharacterCard(props) {
+  return (
+    <Card>
+      <img src={props.image} alt='r&m' />
+      <h3>Name: {props.name}</h3>
+      <p>Species:{props.species}</p>
+      <p>Status: {props.status}</p>      
+      <p>Gender: {props.gender}</p>
+    </Card>
+  );
 }
