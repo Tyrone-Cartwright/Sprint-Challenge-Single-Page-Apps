f2e671ab67f38b1904eab86c3f2ef148ada1936d
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-12-20 16:23:26 -0500

    gave nav basic styling

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import CharacterList from "./components/CharacterList";
 import WelcomePage from "./components/WelcomePage";
 import SearchForm from "./components/SearchForm";
 import { Route, Link } from "react-router-dom";
+import styled from 'styled-components';
 
+const Buttons = styled.div`
+  padding: 10px;
+  margin: 15px;
+  display: inline-block;
+  list-style: none;
+  border: 1px solid #777;
+  border-radius: 5px;
+  box-shadow: 2px 3px #888;
+  color: #000;  
+`
 
 export default function App() {
 
   return (
     <main>
       <nav className="character-list">
-        <Link to="/"><button>Home</button></Link>
-        <Link to="/Search"><button>Search</button></Link>
-        <Link to="/Characters"><button>Characters</button></Link>
+        <Link to="/"><Buttons>Home</Buttons></Link>
+        <Link to="/Search"><Buttons>Search</Buttons></Link>
+        <Link to="/Characters"><Buttons>Characters</Buttons></Link>
       </nav>
       <Header />      
       <Route exact path="/Search" component={SearchForm} />
